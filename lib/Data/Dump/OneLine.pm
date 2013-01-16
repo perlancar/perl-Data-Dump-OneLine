799e31251dfe371c36ff0e0b9d9727e0e4eb14e2
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-01-16 09:37:02 +0700

    dzil: Rearrange to avoid linum change, add # VERSION

--- lib/Data/Dump/OneLine.pm
+++ lib/Data/Dump/OneLine.pm
@@ -1,14 +1,37 @@
 package Data::Dump::OneLine;
-# ABSTRACT: Use Data::Dump to produce dumps that fit in one line
 
 use 5.010;
 use strict;
 use warnings;
+
 use Data::Dump;
 
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(dump_oneline dump_one_line dump1);
+our @EXPORT_OK = qw(dump_oneline dump_one_line dump1 dd1);
+
+# VERSION
+
+sub dump_one_line {
+    local $_ = Data::Dump::dump(@_);
+
+    s/^\s*#.*//mg; # comments
+    s/^\s+//mg; # indents
+    s/\n+/ /g; # newlines
+
+    print STDERR "$_\n" unless defined wantarray;
+    $_;
+}
+
+sub dump1 { dump_one_line(@_) }
+
+# old spelling
+sub dump_oneline { dump_one_line(@_) }
+
+1;
+# ABSTRACT: Use Data::Dump to produce dumps that fit in one line
+
+=for Pod::Coverage ^(dump_oneline)$
 
 =head1 SYNOPSIS
 
@@ -16,6 +39,7 @@ our @EXPORT_OK = qw(dump_oneline dump_one_line dump1);
 
  # just use like Data::Dump's dump()
  dump_one_line(...);
+
 
 =head1 DESCRIPTION
 
@@ -32,35 +56,9 @@ code, etc.
 Dump one or more data structures on a single line. Uses L<Data::Dump> as the
 backend.
 
-=cut
-
-sub dump_one_line {
-    local $_ = Data::Dump::dump(@_);
-
-    s/^\s*#.*//mg; # comments
-    s/^\s+//mg; # indents
-    s/\n+/ /g; # newlines
-
-    print STDERR "$_\n" unless defined wantarray;
-    $_;
-}
-
-1;
-
 =head2 dump1
 
-An alias for dump_one_line().
-
-=cut
-
-sub dump1 { dump_one_line(@_) }
-
-=for Pod::Coverage (dump_oneline)
-
-=cut
-
-# old spelling
-sub dump_oneline { dump_one_line(@_) }
+A shorter alias for dump_one_line().
 
 =head1 SEE ALSO
 
